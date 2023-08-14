4bae1a4d807b6c35795feb2f0e872f6801f86216
PierantonioM <126569384+PierantonioM@users.noreply.github.com>
2023-08-14 12:31:48 +0200

    Errori tb

--- prima_versione.vhd
+++ prima_versione.vhd
@@ -22,7 +22,7 @@ entity project_reti_logiche is
         o_mem_we : out std_logic;
         o_mem_en : out std_logic
     );
-    end project_reti_logiche;
+end project_reti_logiche;
 
 architecture Behavioral of project_reti_logiche is
     type state_type is (reset, waiting_start, reading_z1_bit, reading_address_bits, waiting_mem, loading_data, saving_data, output_data);
@@ -35,7 +35,6 @@ architecture Behavioral of project_reti_logiche is
 
     signal addr_in : std_logic_vector(15 downto 0);
     signal addr_out : std_logic_vector(15 downto 0);
-    signal address_vector : std_logic_vector(15 downto 0);
     signal reg_address : std_logic_vector(15 downto 0) := (others => '0');
     signal mux_addr : std_logic;
 
@@ -62,7 +61,7 @@ begin
     begin
         if(i_rst = '1') then
             bit_0 <= '0';
-        elsif i_clk'event and i_clk = '1' then
+        elsif rising_edge(i_clk) then
             if(b0_save = '1') then
                 bit_0 <= i_w;
             end if;
@@ -73,25 +72,20 @@ begin
     begin
         if(i_rst = '1') then
             bit_1 <= '0';
-        elsif i_clk'event and i_clk = '1' then
+        elsif rising_edge(i_clk) then
             if(b1_save = '1') then
                 bit_1 <= i_w;
             end if;
         end if;
     end process;
 
-    addr_in <= ("000000000000000" & i_w) + (reg_address(14 downto 0) & '0');
+    addr_in <= std_logic_vector((unsigned("000000000000000" & i_w) + (unsigned(reg_address(14 downto 0) & '0'))));
 
-    with mux_addr select
-        addr_out <= "0000000000000000" when '0',
-                    addr_in when '1',
-                    "XXXXXXXXXXXXXXXX" when others;
-    
     process(i_clk, i_rst)
     begin
         if(i_rst = '1') then
             reg_address <= "0000000000000000";
-        elsif i_clk'event and i_clk = '1' then
+        elsif rising_edge(i_clk) then
             if(addr_save = '1') then
                 reg_address <= addr_out;
             end if;
@@ -100,47 +94,50 @@ begin
     
     process(i_clk, i_rst)
     begin
-        z0_save <= '0';
-        z1_save <= '0';
-        z2_save <= '0';
-        z3_save <= '0';
-        case (bit_0 & bit_1) is
-            when "00" =>
-                z0_save <= out_save;
-            when "01" =>
-                z1_save <= out_save;
-            when "10" =>
-                z2_save <= out_save;
-            when "11" =>
-                z3_save <= out_save;
-            when others =>
-            z0_save <= 'X';
-            z1_save <= 'X';
-            z2_save <= 'X';
-            z3_save <= 'X';
-        end case;
+        if(i_rst = '1') then
+            z0_save <= '0';
+            z1_save <= '0';
+            z2_save <= '0';
+            z3_save <= '0';
+        elsif rising_edge(i_clk) then
+            case (bit_0 & bit_1) is
+                when "00" =>
+                    z0_save <= out_save;
+                when "01" =>
+                    z1_save <= out_save;
+                when "10" =>
+                    z2_save <= out_save;
+                when "11" =>
+                    z3_save <= out_save;
+                when others =>
+                    z0_save <= 'X';
+                    z1_save <= 'X';
+                    z2_save <= 'X';
+                    z3_save <= 'X';
+            end case;
+        end if;
     end process;
     
-    --Come leggere e scrivere dalla memoria nel registro reg_data
+    -- Come leggere e scrivere dalla memoria nel registro reg_data
     o_mem_addr <= reg_address;
 
     process(i_clk, i_rst)
     begin
         if(i_rst = '1') then
             reg_data <= "00000000";
-        elsif i_clk'event and i_clk = '1' then
+        elsif rising_edge(i_clk) then
             if(data_save = '1') then               
                 reg_data <= i_mem_data;
             end if;
         end if;
     end process;
-    --Fine memoria
+    -- Fine memoria
 
     process(i_clk, i_rst)
     begin
         if(i_rst = '1') then
             o_z0_reg <= "00000000";
-        elsif i_clk'event and i_clk = '1' then
+        elsif rising_edge(i_clk) then
             if(z0_save = '1') then
                 o_z0_reg <= reg_data;
             end if;
@@ -151,7 +148,7 @@ begin
     begin
         if(i_rst = '1') then
             o_z1_reg <= "00000000";
-        elsif i_clk'event and i_clk = '1' then
+        elsif rising_edge(i_clk) then
             if(z1_save = '1') then
                 o_z1_reg <= reg_data;
             end if;
@@ -162,7 +159,7 @@ begin
     begin
         if(i_rst = '1') then
             o_z2_reg <= "00000000";
-        elsif i_clk'event and i_clk = '1' then
+        elsif rising_edge(i_clk) then
             if(z2_save = '1') then
                 o_z2_reg <= reg_data;
             end if;
@@ -173,7 +170,7 @@ begin
     begin
         if(i_rst = '1') then
             o_z3_reg <= "00000000";
-        elsif i_clk'event and i_clk = '1' then
+        elsif rising_edge(i_clk) then
             if(z3_save = '1') then
                 o_z3_reg <= reg_data;
             end if;
@@ -190,11 +187,11 @@ begin
     begin
         if(i_rst = '1') then
             cur_state <= reset;
-        elsif i_clk'event and i_clk = '1' then
+        elsif rising_edge(i_clk) then
             cur_state <= next_state;
         end if;
     end process;
-
+    
     process(cur_state, i_start)
     begin
         next_state <= cur_state;
@@ -253,6 +250,8 @@ begin
 
         case cur_state is
             when reset =>
+                -- Inizializzazioni dello stato di reset
+                -- Assegnare valori iniziali ai segnali, se necessario
             when waiting_start =>
                 b0_save <= '1';
             when reading_z1_bit =>
@@ -269,7 +268,7 @@ begin
                 out_save <= '1';
             when output_data =>
                 o_done <= '1';
-            end case;
+        end case;
     end process;
 
 end Behavioral;
