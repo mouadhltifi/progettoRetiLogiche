e42e23e4645865207eb49691ed030615aef408da
PierantonioM <126569384+PierantonioM@users.noreply.github.com>
2023-08-20 20:08:06 +0200

    prima_versione supera tb

--- prima_versione.vhd
+++ prima_versione.vhd
@@ -25,7 +25,7 @@ entity project_reti_logiche is
 end project_reti_logiche;
 
 architecture Behavioral of project_reti_logiche is
-    type state_type is (reset, waiting_start, reading_z1_bit, reading_address_bits, waiting_mem, loading_data, saving_data, output_data);
+    type state_type is (reset, waiting_start, reading_z1_bit, reading_address_bits, waiting_mem, loading_data, selecting_channel, saving_data, output_data);
     signal cur_state, next_state : state_type;
 
     signal init : STD_LOGIC;
@@ -53,6 +53,8 @@ architecture Behavioral of project_reti_logiche is
     signal z2_save : std_logic;
     signal z3_save : std_logic;
 
+    signal clean_out : std_logic;
+    signal write_out : std_logic;
     signal o_z0_reg : std_logic_vector(7 downto 0);
     signal o_z1_reg : std_logic_vector(7 downto 0);
     signal o_z2_reg : std_logic_vector(7 downto 0);
@@ -107,10 +109,12 @@ begin
     
     process(i_clk, i_rst)
     begin
-    z0_save <= '0';
-    z1_save <= '0';
-    z2_save <= '0';
-    z3_save <= '0';
+        if(init = '1') then
+            z0_save <= '0';
+            z1_save <= '0';
+            z2_save <= '0';
+            z3_save <= '0';
+        end if;
         if(i_rst = '1') then
             z0_save <= '0';
             z1_save <= '0';
@@ -228,10 +232,26 @@ begin
         end if;
     end process;
 
-    o_z0 <= o_z0_reg;
-    o_z1 <= o_z1_reg;
-    o_z2 <= o_z2_reg;
-    o_z3 <= o_z3_reg;
+    with write_out select
+        o_z0 <= "00000000" when '0',
+                o_z0_reg when '1',
+                "XXXXXXXX" when others;
+
+    with write_out select
+        o_z1 <= "00000000" when '0',
+                o_z1_reg when '1',
+                "XXXXXXXX" when others;
+                
+    with write_out select
+        o_z2 <= "00000000" when '0',
+                o_z2_reg when '1',
+                "XXXXXXXX" when others;
+                
+    with write_out select
+        o_z3 <= "00000000" when '0',
+                o_z3_reg when '1',
+                "XXXXXXXX" when others;
+        
 
     -- State Machine
     process(i_clk, i_rst)
@@ -247,20 +267,23 @@ begin
     begin
         next_state <= cur_state;
 
-            if i_rst = '1' then
-                next_state <= reset;
-            else
+           
                 case cur_state is
                     when reset =>
                         next_state <= waiting_start;
                     
                     when waiting_start =>
-                        if i_start = '1' then
+                        if i_rst ='1' then
+                            next_state <= reset;
+                        elsif i_start = '1' then
                             next_state <= reading_z1_bit;
                         else                            
                             next_state <= waiting_start;                      
                         end if;
 
+                    --when reading_z0_bit =>
+                    --    next_state <= reading_z1_bit;
+                    
                     when reading_z1_bit =>
                     if i_start ='1' then
                         next_state <= reading_address_bits;
@@ -279,16 +302,23 @@ begin
                         next_state <= loading_data;
 
                     when loading_data =>
+                        next_state <= selecting_channel;
+
+
+                    when selecting_channel =>
                         next_state <= saving_data;
 
                     when saving_data =>
                         next_state <= output_data;
 
                     when output_data =>
-                        next_state <= waiting_start;
+                        if i_rst ='1' then
+                            next_state <= reset;
+                        else
+                            next_state <= waiting_start;
+                        end if;
 
                 end case;
-            end if;
     end process;
 
     process(cur_state)
@@ -303,12 +333,20 @@ begin
         o_done <= '0';
         o_mem_we <= '0';
         o_mem_en <= '0';
+        clean_out <= '0';
+        write_out <= '0';
 
         case cur_state is
             when reset =>
                 init <= '1';
+                clean_out <= '1';
             when waiting_start =>
+                clean_out <= '1';
+                mux_addr <= '0';
+                addr_save <= '1';
                 b0_save <= '1';
+            --when reading_z0_bit =>
+            --    b0_save <= '1';
             when reading_z1_bit =>
                 b1_save <= '1';
             when reading_address_bits =>
@@ -319,10 +357,14 @@ begin
             when loading_data =>
                 o_mem_en <= '1';
                 data_save <= '1';
+            when selecting_channel =>
+                out_save <= '1';
             when saving_data =>
                 out_save <= '1';
+                write_out <= '1';
             when output_data =>
                 o_done <= '1';
+                write_out <= '1';
         end case;
     end process;
 
